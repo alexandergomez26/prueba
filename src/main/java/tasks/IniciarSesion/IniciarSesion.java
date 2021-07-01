bef2819bd8f724c61425a3e5320b2a673ee90ca9
Jose Alexander Gomez <jgomezl@proteccion.com.co>
2021-07-01 13:21:00 -0500

    Se hace captura de Jenkins

--- src/main/java/tasks/IniciarSesion/IniciarSesion.java
+++ src/main/java/tasks/IniciarSesion/IniciarSesion.java
@@ -7,11 +7,16 @@ import models.DatosInicioSesion;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.conditions.Check;
 import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
 import net.serenitybdd.screenplay.waits.WaitUntil;
+import org.openqa.selenium.By;
+import java.util.concurrent.TimeUnit;
+
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 import static userinterfaces.IniciarSesionPage.*;
 
 public class IniciarSesion implements Task {
@@ -25,17 +30,31 @@ public class IniciarSesion implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
+
+        BrowseTheWeb.as(theActorInTheSpotlight()).getDriver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        String html2 = BrowseTheWeb.as(theActorInTheSpotlight()).getDriver().findElement(By.cssSelector("html")).getAttribute("innerHTML");
+        System.out.println("----------------------------------------------------------------------------------------------------------");
+        System.out.println(html2);
+        System.out.println("----------------------------------------------------------------------------------------------------------");
+
         actor.attemptsTo(
-              Espera.cantidadDeMiliSegundos(5000),
-        Enter.theValue(datosInicioSesion.getCorreo()).into(TXT_CORREO),
-        Click.on(BTN_SIGUIENTE),
-        (WaitUntil.the(TXT_CONTRASENIA, WebElementStateMatchers.isVisible()).
+                Enter.theValue(datosInicioSesion.getCorreo()).into(TXT_CORREO));
+
+        BrowseTheWeb.as(theActorInTheSpotlight()).getDriver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        String html3 = BrowseTheWeb.as(theActorInTheSpotlight()).getDriver().findElement(By.cssSelector("html")).getAttribute("innerHTML");
+        System.out.println("----------------------------------------------------------------------------------------------------------");
+        System.out.println(html3);
+        System.out.println("----------------------------------------------------------------------------------------------------------");
+
+        actor.attemptsTo(
+                Click.on(BTN_SIGUIENTE),
+                (WaitUntil.the(TXT_CONTRASENIA, WebElementStateMatchers.isVisible()).
                         forNoMoreThan(10).seconds()),
-        Enter.theValue(datosInicioSesion.getContrasenia()).into(TXT_CONTRASENIA),
-        Click.on(BTN_INICIAR_SESION),
-        Check.whether(BTN_NO_MANTENER_SESION_INICIADA.resolveFor(actor).
-                isVisible()).andIfSo(Click.on(BTN_NO_MANTENER_SESION_INICIADA)).otherwise(),
-        Espera.cantidadDeMiliSegundos(5000)
+                Enter.theValue(datosInicioSesion.getContrasenia()).into(TXT_CONTRASENIA),
+                Click.on(BTN_INICIAR_SESION),
+                Check.whether(BTN_NO_MANTENER_SESION_INICIADA.resolveFor(actor).
+                        isVisible()).andIfSo(Click.on(BTN_NO_MANTENER_SESION_INICIADA)).otherwise(),
+                Espera.cantidadDeMiliSegundos(5000)
         );
     }
 
