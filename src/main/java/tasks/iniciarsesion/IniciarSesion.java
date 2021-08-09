d6560e8675f42484f11e6c71ac0b86ea57253d72
Jose Alexander Gomez <jgomezl@proteccion.com.co>
2021-08-09 16:46:24 -0500

    Se modifica clase IniciarSesion

--- src/main/java/tasks/iniciarsesion/IniciarSesion.java
+++ src/main/java/tasks/iniciarsesion/IniciarSesion.java
@@ -1,5 +1,6 @@
 package tasks.iniciarsesion;
 
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import utils.constantes.ConstantesGenerales;
 import utils.exceldata.CreateModels;
 import interactions.Espera;
@@ -12,9 +13,14 @@ import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import static userinterfaces.IniciarSesionPage.*;
 
 public class IniciarSesion implements Task {
+
+    private static final Logger logger = Logger.getLogger("co.com.viliv.interactions.Wait");
 
     private final DatosInicioSesion datosInicioSesion;
 
@@ -38,30 +44,34 @@ public class IniciarSesion implements Task {
                 WaitUntil.the(TXT_CORREO, WebElementStateMatchers.isVisible()).
                         forNoMoreThan(60).seconds(),
                 Enter.theValue(datosInicioSesion.getCorreo()).into(TXT_CORREO),
-                Click.on(BTN_SIGUIENTE)
+                Click.on(BTN_SIGUIENTE),
+                Espera.cantidadDeMiliSegundos(10000)
         );
 
-        actor.attemptsTo(
-                Espera.cantidadDeMiliSegundos(25000)
-        );
-/*
-        BrowseTheWeb.as(theActorInTheSpotlight()).getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        String html1 = BrowseTheWeb.as(theActorInTheSpotlight()).getDriver().findElement(By.cssSelector("html")).getAttribute("innerHTML");
-        System.out.println("----------------------------------------------------------------------------------------------------------");
-        System.out.println(html1);
-        System.out.println("----------------------------------------------------------------------------------------------------------");
+        Boolean contrasenia = TXT_CONTRASENIA.resolveFor(actor).isVisible();
 
- */
+        if (contrasenia.equals(false)) {
+            for (int intento = 1; intento <= 4; intento++) {
+                BrowseTheWeb.as(actor).getDriver().navigate().refresh();
+                actor.attemptsTo(
+                        WaitUntil.the(TXT_CORREO, WebElementStateMatchers.isVisible()).
+                                forNoMoreThan(60).seconds(),
+                        Enter.theValue(datosInicioSesion.getCorreo()).into(TXT_CORREO),
+                        Click.on(BTN_SIGUIENTE),
+                        Espera.cantidadDeMiliSegundos(5000));
+                logger.log(Level.INFO, "El resultado es: " + contrasenia + ", Reintento # " + intento);
+            }
 
+        }
         actor.attemptsTo(
-        WaitUntil.the(TXT_CONTRASENIA, WebElementStateMatchers.isVisible()).
-                        forNoMoreThan(40).seconds(),
+                WaitUntil.the(TXT_CONTRASENIA, WebElementStateMatchers.isVisible()).
+                        forNoMoreThan(40).seconds());
+        actor.attemptsTo(
                 Enter.theValue(datosInicioSesion.getContrasenia()).into(TXT_CONTRASENIA),
                 Click.on(BTN_INICIAR_SESION),
                 WaitUntil.the(BTN_NO_MANTENER_SESION_INICIADA, WebElementStateMatchers.isVisible()).
                         forNoMoreThan(60).seconds(),
-                Click.on(BTN_NO_MANTENER_SESION_INICIADA),
-                Espera.cantidadDeMiliSegundos(5000)
+                Click.on(BTN_NO_MANTENER_SESION_INICIADA)
         );
 /*
         BrowseTheWeb.as(theActorInTheSpotlight()).getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
